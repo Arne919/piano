3714e43dba34743c257fc6ab834a38991a6723e6
Kang hye kyoung <ghgghg96@naver.com>
2025-03-07 10:20:47 +0900

    fix: 로컬에서는 작동되는데 웹에서는 안되는이유?

--- piano.js
+++ piano.js
@@ -1,20 +1,27 @@
+let audioContext;
+
+document.addEventListener("click", () => {
+    if (!audioContext) {
+        audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    }
+}, { once: true });  // 사용자가 한 번 클릭하면, 이후 오디오 정상 작동
+
 // 각 키에 해당하는 소리 파일 매핑
 const sounds = {
-  C4: "sounds/C4.mp3",
-  "C#4": "sounds/C-sharp4.mp3",  // 변경됨
-  D4: "sounds/D4.mp3",
-  "D#4": "sounds/D-sharp4.mp3",  // 변경됨
-  E4: "sounds/E4.mp3",
-  F4: "sounds/F4.mp3",
-  "F#4": "sounds/F-sharp4.mp3",  // 변경됨
-  G4: "sounds/G4.mp3",
-  "G#4": "sounds/G-sharp4.mp3",  // 변경됨
-  A4: "sounds/A4.mp3",
-  "A#4": "sounds/A-sharp4.mp3",  // 변경됨
-  B4: "sounds/B4.mp3",
-  C5: "sounds/C5.mp3"
+  C4: "./sounds/C4.mp3",
+  "C#4": "./sounds/C-sharp4.mp3",
+  D4: "./sounds/D4.mp3",
+  "D#4": "./sounds/D-sharp4.mp3",
+  E4: "./sounds/E4.mp3",
+  F4: "./sounds/F4.mp3",
+  "F#4": "./sounds/F-sharp4.mp3",
+  G4: "./sounds/G4.mp3",
+  "G#4": "./sounds/G-sharp4.mp3",
+  A4: "./sounds/A4.mp3",
+  "A#4": "./sounds/A-sharp4.mp3",
+  B4: "./sounds/B4.mp3",
+  C5: "./sounds/C5.mp3"
 };
-
 
 // 마우스로 클릭하면 소리 재생
 document.querySelectorAll(".key").forEach(key => {
@@ -45,6 +52,21 @@ document.addEventListener("keydown", (event) => {
 
 // 사운드 재생 함수
 function playSound(note) {
-  const audio = new Audio(sounds[note]);
-  audio.play();
+    if (!audioContext) {
+        audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    }
+
+    const soundFile = sounds[note];
+    if (!soundFile) return;
+
+    fetch(soundFile)
+        .then(response => response.arrayBuffer())
+        .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
+        .then(audioBuffer => {
+            const source = audioContext.createBufferSource();
+            source.buffer = audioBuffer;
+            source.connect(audioContext.destination);
+            source.start(0);
+        })
+        .catch(error => console.error("오디오 재생 오류:", error));
 }
